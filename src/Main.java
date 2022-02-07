cb3127a2aa626c64715f5ebb78939bef2e1f23a5
Mehmet CEBESOY <mhmtcbsy@gmail.com>
2022-02-07 23:22:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// for i++ 1 artır demek
+        for (int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+        System.out.println ("Döngü bitti");
+        // 10 a kadar olan tek sayıları yazdırmak için
+        for (int i=1; i<=10; i+=2){
+            System.out.println(i);
+        }
+    }
+}
